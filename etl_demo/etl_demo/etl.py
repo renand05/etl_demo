9d9bed366e036d69284df9304ca20a42e300ff36
Andres Rengifo <amrengifoh@gmail.com>
2023-12-10 22:21:17 -0500

    feat: include tests

--- etl_demo/etl_demo/etl.py
+++ etl_demo/etl_demo/etl.py
@@ -2,28 +2,18 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 
 def run_etl():
-    # Initialize a Spark session
     spark = SparkSession.builder \
         .appName("ETL Demo") \
         .getOrCreate()
 
     try:
-        # Load data (replace with your data source)
-        input_data = [("Alice", 25), ("Bob", 30), ("Charlie", 22)]
-        df = spark.createDataFrame(input_data, ["Name", "Age"])
+        csv_file_path = "etl_demo/demo_data/Nevada_Dept_of_Public_Behavioral_Health.csv"
 
-        # Transform data (example transformation: add 5 to the age)
-        transformed_df = df.withColumn("AgePlus5", col("Age") + 5)
-
-        # Show the transformed DataFrame
+        df = spark.read.csv(csv_file_path, header=True, inferSchema=True)
         print("Transformed DataFrame:")
-        transformed_df.show()
-
-        # Write the transformed data to an output location (replace with your destination)
-        transformed_df.write.mode("overwrite").parquet("output_data")
+        df.show()
 
     finally:
-        # Stop the Spark session to release resources
         spark.stop()
 
 if __name__ == "__main__":
